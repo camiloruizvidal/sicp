548d94f6b10e4a5242145ba15f4e2d7aa07bf258
camiloruizvidal <camiloruizvidal@gmail.com>
2017-12-27 23:12:32 -0500

    #136
    Actualizacion importante

---
+++ sql/Actualizaciones/2017-12-27-1.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+  `tbl_persona_familiaridad`
+  (`id_persona_familiaridad`,`descripcion`,`sexo`)
+VALUES
+(13,"Nieto","m"),
+(14,"Nieta","f"),
+(15,"Bisnieto","m"),
+(16,"Bisnieta","f");
